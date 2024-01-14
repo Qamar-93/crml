7b206ae0deb2525f4809483edd29242555eddc3a
Qamar Alfalojy <qmff23@gmail.com>
2024-01-14 23:21:41 +0100

    distance functions and plots

---
+++ code/utils/dists.py
@@ -0,0 +1,180 @@
+from matplotlib.pylab import figure
+import matplotlib.pylab as plt
+import os
+import similaritymeasures as sm
+from scipy.spatial.distance import directed_hausdorff
+import numpy as np
+
+### calculate distance between each point and the max, each point and the min
+def calculate_ditances(x, x_max, x_min):
+    distsmax, distsmin = [], []
+    distsmax = abs(x_max - x)
+    distsmin = abs(x_min - x)
+
+    return [distsmax, distsmin]
+
+
+## calculate cosine distance between each point and the max, each point and the min
+def calculate_cosine_distance(x, y, domain="waveform"):
+    # calculate the cosine similarity
+    if len(x) != len(y):
+    # pad the shorter vector with zeros
+        if len(x) < len(y):
+            x = np.concatenate((x, np.zeros(len(y) - len(x))))
+        else:
+            y = np.concatenate((y, np.zeros(len(x) - len(y))))
+    if domain =="waveform":
+        cs = np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+        return (1 - cs)/ 2
+    else:
+        from sklearn.metrics.pairwise import cosine_distances
+        cs = cosine_distances(x, y)
+        print("cs", np.mean(cs))
+        return np.mean(cs).tolist()
+def dtw(x, y):
+    dist, path = dtw(x.T, y.T)
+    dtw_distance = dist / len(path)
+    # return sm.dtw(x, y, keep_internals=True, distance_only=True)
+    return dtw_distance
+
+## calculate the point-wise cosine distance between each point and the max, each point and the min
+def point_wise_cosine_distance(x, x_max, x_min):
+    distsmax, distsmin = [], []
+    from scipy.spatial.distance import cosine 
+
+    for idx, item in enumerate(x):
+        distsmax.append(1 - cosine(np.array(x_max[idx]), np.array(item)))
+        distsmin.append(1 - cosine(np.array(x_min[idx]), np.array(item)))
+    
+    return [distsmax, distsmin]
+
+## plot distances between min and max at each point as vertical lines
+def plot_distances(x, x_min, x_max, vis=False, save=True, fig_name="Distances plot", path="./" ):
+    
+    figure(figsize=(8,8))
+    
+    for idx, point in enumerate(x):
+      plt.vlines(point, ymin=x_min[idx], ymax = x_max[idx])
+
+    plt.xlabel("X", fontsize=18)
+    plt.ylabel("Y", fontsize=18)
+    plt.title("Differences between min and max")
+    plt.legend(prop={'size': 14}, shadow=True, handlelength=1.5, fontsize=14)
+
+    if save:
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        plt.savefig(f"{path}/{fig_name}.pdf")
+        if vis:
+            plt.show()
+            plt.clf()
+    if vis:
+        plt.show()
+        plt.clf()
+
+
+"""
+plot max distance such that:
+for every xi: yi= max(dist(xi, max), d(xi,min))
+"""  
+def plot_max_distance(x, distances_array, dist_min, dist_max, x_min, x_max, vis=False, save=True, fig_name="Max distances", path="./"):
+    plt.clf()
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    distsminmax_y = []
+    maxaxs = []
+    handelA, handelB = None, None
+    for idx, point in enumerate(x):
+        if distances_array[idx] == dist_min[idx]:
+            distsminmax_y.append(x_min[idx])
+        # handelA = plt.vlines(point, ymin=x_min[idx], ymax = x_min[idx]+dist_min[idx], colors='g')
+            handelA = plt.vlines(point, ymin=x_min[idx], ymax = x_min[idx]+dist_min[idx], colors='g', alpha=0.1)
+        else:
+            distsminmax_y.append(x_max[idx])
+            handelB =plt.vlines(point, ymin=x_max[idx]-dist_max[idx], ymax =x_max[idx] , colors='r', alpha=0.1)
+        
+    plt.xlabel("X", fontsize=18)
+    plt.ylabel("Y", fontsize=18)
+
+    plt.title("for every xi: yi = max(d(xi, max), d(xi,min))")
+
+    plt.legend(prop={'size': 14}, handles=[handelA, handelB], labels=["min is greater", "max is greater"], fontsize=14)
+    if save:
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        plt.savefig(f"{path}/{fig_name}.pdf",bbox_inches='tight')
+    if vis:
+        plt.show()    
+
+
+def plot_max_distance_curves(x, true_sloution, distances_array, dist_min, dist_max, x_min, x_max, vis=False, save=True, fig_name="Max distances curve", path="./"):
+    plt.clf()
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    distsminmax_y = []
+    maxaxs = []
+    handelA, handelB = None, None
+    for idx, point in enumerate(x):
+        if distances_array[idx] == dist_min[idx]:
+            distsminmax_y.append(x_min[idx])
+            handelA = plt.vlines(point, ymin=x_min[idx], ymax = x_min[idx]+dist_min[idx], colors='g', alpha=0.3)
+        else:
+            distsminmax_y.append(x_max[idx])
+            handelB =plt.vlines(point, ymin=x_max[idx]-dist_max[idx], ymax =x_max[idx] , colors='r', alpha=0.3)
+        
+    plt.xlabel("X", fontsize=18)
+    plt.ylabel("Y", fontsize=18)
+
+    plt.title("for every xi: yi = max(d(xi, max), d(xi,min))")
+
+    handelC = plt.plot(x, distsminmax_y, label = "Max. distances curve", alpha =1)
+    handelD = plt.plot(x, true_sloution, label = "True solution", alpha=1)
+    plt.legend(prop={'size': 14}, shadow=True, fontsize=14)
+
+
+    if save:
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        plt.savefig(f"{path}/{fig_name}.pdf", bbox_inches='tight')
+        plt.close()
+    if vis:
+        plt.show()
+    
+    return distsminmax_y
+
+
+def min_max_bounds(df):
+    
+    min_bound = np.array(df.min(axis=1))
+    max_bound = np.array(df.max(axis=1))
+    
+    return min_bound, max_bound
+
+
+# Euclidean distance
+def L2_distance(x, y, type="pointwise"):
+    if type == "pointwise":
+        return [np.sqrt((a-b)*(a-b)) for a, b in zip(x, y)]
+    else:
+        return np.sqrt(np.sum([(a-b)*(a-b) for a, b in zip(x, y)]))
+
+# Frechet distance
+def frechet_distance(x, y):
+    xaxis = np.arange(0, len(x))
+    repart_x = np.column_stack((xaxis, x))
+    repart_y = np.column_stack((xaxis, y))
+
+    return sm.frechet_dist(repart_x, repart_y)
+
+# Hausdorff distance
+def hausdorff_distance(x, y):
+    xaxis = np.arange(0, len(x))
+    repart_x = np.column_stack((xaxis, x))
+    repart_y = np.column_stack((xaxis, y))
+    return directed_hausdorff(repart_x, repart_y)[0]
+    
+def L1_distance(x, y, type="pointwise"):
+    if type == "pointwise":
+        return abs(x-y)
+    else:
+        return np.sum([abs(a-b) for a, b in zip(x, y)])
---
+++ code/utils/helpers.py
@@ -0,0 +1,229 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+# Helper function to get the data based on the type
+def get_data(data_types, x_clean, y_clean, gx, gy):
+    """
+    Concatenates the data based on the specified data types.
+
+    Parameters:
+    data_types (list): A list of data types to include in the concatenated data.
+    x_clean (ndarray): The clean x data.
+    y_clean (ndarray): The clean y data.
+    gx (ndarray): The gx data.
+    gy (ndarray): The gy data.
+
+    Returns:
+    ndarray: The concatenated x data.
+    ndarray: The concatenated y data.
+    """
+    x_data, y_data = [], []
+    for data_type in data_types:
+        if data_type == 'clean':
+            x_data.append(x_clean)
+            y_data.append(y_clean)
+        elif data_type == 'gx':
+            x_data.append(gx)
+            y_data.append(gy)
+        
+    return np.concatenate(x_data), np.concatenate(y_data)
+
+def get_data_from_dict(config, x_clean, y_clean, gx, gx_y):
+    """
+    config (dict): A dictionary of data types to include in the concatenated data.
+    x_clean (ndarray): The clean x data.
+    y_clean (ndarray): The clean y data.
+    gx (ndarray): The gx data.
+    gy (ndarray): The gy data.
+    returns:
+    x_train (ndarray): The concatenated x data for training.
+    y_train (ndarray): The concatenated y data for training.
+    x_valid (ndarray): The concatenated x data for validation.
+    y_valid (ndarray): The concatenated y data for validation.
+    x_test (ndarray): The concatenated x data for testing.
+    y_test (ndarray): The concatenated y data for testing.
+    """
+    # copy the config dictionary to avoid changing the original one
+    data_dict = config.copy()
+   
+   
+    # add indices column to x_clean, so that we can know which indices were used for training, validation, and testing
+    indices = np.arange(x_clean.shape[0])
+    
+        
+    # x_train, x_temp, y_train, y_temp = train_test_split(x_clean, y_clean, test_size=0.2, random_state=42, shuffle=False)
+    indices_train, indices_temp, x_train, x_temp, y_train, y_temp = train_test_split(indices, x_clean, y_clean, test_size=0.2, random_state=42)
+    
+    # Split the temporary set into validation and testing sets
+    # x_valid, x_test, y_valid, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=42, shuffle=False
+    # )
+    indices_valid, indices_test, x_valid, x_test, y_valid, y_test = train_test_split(indices_temp, x_temp, y_temp, test_size=0.5, random_state=42)
+    
+    # ####### no shuffle split     
+    # x_train, x_temp, y_train, y_temp = train_test_split(x_clean, y_clean, test_size=0.2, random_state=42, shuffle=False)
+    # # Split the temporary set into validation and testing sets
+    # x_valid, x_test, y_valid, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=42, shuffle=False)
+
+    ###### the following split is for the old code with get_data, the last three commented lines in this function
+    # x_train_clean, x_temp_clean, y_train_clean, y_temp_clean = train_test_split(x_clean, y_clean, test_size=0.1, random_state=42, shuffle=False)
+    # print("x_train_clean shape is", x_train_clean.shape, "y_train_clean shape is", y_train_clean.shape, "x_valid shape is", x_valid.shape, "y_valid shape is", y_valid.shape, "x_test shape is", x_test.shape, "y_test shape is", y_test.shape)
+    
+    # if training contains gx, then add it as a new feature column for training dataset
+          # remove gx from all elements of the dictionary
+    for key in data_dict:
+        if key == 'training' and 'gx' in data_dict[key]:
+            # concatenate x_train with gx of training_indices as a new column feature
+            x_train = np.concatenate((x_train, gx[indices_train]), axis=1)
+            # now double the length of x_train by repeating it, setting the gx values to 0
+            x_train_new = np.concatenate((x_train, x_train), axis=0)
+            x_train_new[:, -gx.shape[1]:] = 0
+            x_train = x_train_new
+            # now repeat y_train as well
+            y_train = np.concatenate((y_train, y_train), axis=0)
+            # for the validation set, add new column feature for gx only with values 0
+            gx_valid = np.zeros((x_valid.shape[0], gx.shape[1]))
+            x_valid = np.concatenate((x_valid, gx_valid), axis=1)
+            # append the last values of gx to the test set, from the end back the length of x_test
+            x_test = np.concatenate((x_test, gx[indices_test]), axis=1)
+            x_test = np.concatenate((x_test, x_test), axis=0)
+            
+            y_test = np.concatenate((y_test, y_test), axis=0)
+            data_dict[key] = [x for x in data_dict[key] if x != 'gx']
+            
+                     
+        else:
+            gx_train, gx_temp, gy_train, gy_temp = train_test_split(gx, gx_y, test_size=0.2, random_state=42)
+            gx_valid, gx_test, gy_valid, gy_test = train_test_split(gx_temp, gy_temp, test_size=0.5, random_state=42)
+ 
+    # x_train, y_train = get_data(data_dict['training'], x_train_clean, y_train_clean, gx_train, gy_train)
+
+    # x_valid, y_valid = get_data(data_dict['validation'], x_temp_clean, y_temp_clean, gx_valid, gy_valid)
+
+    # x_test, y_test = get_data(data_dict['testing'], x_temp_clean, y_temp_clean, gx_test, gy_test)
+    return x_train, y_train, x_valid, y_valid, x_test, y_test
+
+def noise_aware_data(config, x_clean, y_clean, gx, gx_y):
+    """
+    config (dict): A dictionary of data types to include in the concatenated data.
+    x_clean (ndarray): The clean x data.
+    y_clean (ndarray): The clean y data.
+    gx (ndarray): The gx data.
+    gy (ndarray): The gy data.
+    returns:
+    x_train (ndarray): The concatenated x data for training.
+    y_train (ndarray): The concatenated y data for training.
+    x_valid (ndarray): The concatenated x data for validation.
+    y_valid (ndarray): The concatenated y data for validation.
+    x_test (ndarray): The concatenated x data for testing.
+    y_test (ndarray): The concatenated y data for testing.
+    """
+    # copy the config dictionary to avoid changing the original one
+    data_dict = config.copy()
+   
+    indices = np.arange(x_clean.shape[0])
+         
+    indices_train, indices_temp, x_train, x_temp, y_train, y_temp = train_test_split(indices, x_clean, y_clean, test_size=0.2, random_state=42)
+   
+    indices_valid, indices_test, x_valid, x_test, y_valid, y_test = train_test_split(indices_temp, x_temp, y_temp, test_size=0.5, random_state=42)
+   
+    # ####### no shuffle split     
+    # x_train, x_temp, y_train, y_temp = train_test_split(x_clean, y_clean, test_size=0.2, random_state=42, shuffle=False)
+    # # Split the temporary set into validation and testing sets
+    # x_valid, x_test, y_valid, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=42, shuffle=False)
+
+    
+    # creat gx_training, which is gx with the same length as x_train, and zeros for another training size
+    gx_train = np.concatenate((gx[indices_train], np.zeros((x_train.shape[0], gx.shape[1]))), axis=0)
+    x_train_new = np.concatenate((x_train, x_train), axis=0)
+    x_train = np.concatenate((x_train_new, gx_train), axis=1)
+
+    # repeat y_train as well
+    y_train = np.concatenate((y_train, y_train), axis=0)
+
+    # for the validation set, add new column feature for gx only with values 0
+    gx_valid = np.zeros((x_valid.shape[0], gx.shape[1]))
+    x_valid = np.concatenate((x_valid, gx_valid), axis=1)
+    
+    # append the last values of gx to the test set, from the end back the length of x_test
+    x_test = np.concatenate((x_test, gx[indices_test]), axis=1)
+    x_test = np.concatenate((x_test, x_test), axis=0)
+    
+    y_test = np.concatenate((y_test, y_test), axis=0)
+
+    return x_train, y_train, x_valid, y_valid, x_test, y_test, indices_train, indices_valid
+
+def get_adversarial_data(config, x_clean, y_clean, gx, gx_y):
+    """
+    the adversarial data is split clean data for training and testing, and use gx for validation
+    """
+    indices = np.arange(x_clean.shape[0])
+    indices_train, indices_test, x_train, x_test, y_train, y_test = train_test_split(indices, x_clean, y_clean, test_size=0.2, random_state=42)
+    x_valid, x_test, y_valid, y_test = train_test_split(gx, gx_y, test_size=0.5, random_state=42)
+    return x_train, y_train, x_valid, y_valid, x_test, y_test, indices_train, indices
+
+def evaluate(model, x):
+    """
+    Evaluate the model on the given data.
+
+    Parameters:
+    model (object): The trained model.
+    x (ndarray): The input data.
+
+    Returns:
+    ndarray: The predicted values.
+    """
+    return model.predict(x)
+
+def evaluate_and_plot(model, history, xy_test, path="./"):
+    """
+    Evaluate the model on the test data, generate predictions, and plot the predicted vs actual values and loss history.
+
+    Parameters:
+    model (object): The trained model.
+    history (object): The training history of the model.
+    xy_test (tuple): A tuple containing the test data (x_test, y_test).
+    path (str): The path to save the generated plots. Default is the current directory.
+
+    Returns:
+    None
+    """
+    # Evaluate the model on the test data
+    x_test = xy_test[0]
+    y_test = xy_test[1]
+    print("x_test shape: ", x_test.shape)
+    print("y_test shape: ", y_test.shape)
+    
+    test_loss = model.evaluate(x_test, y_test)
+    # Generate predictions on the test data
+    y_pred = model.predict(x_test)
+    if y_pred.shape[1] == 1:
+        # Plot the predicted vs actual values
+        plt.figure(figsize=(10, 6))
+        plt.scatter(y_test, y_pred, label='Actual vs Predicted')
+        # plt.plot(np.linspace(-3, 3, len(y_test)), y_test, label='Actual')
+        # plt.plot(np.linspace(-3, 3, len(y_pred)), y_pred, label='Predicted')
+        plt.title('Predicted vs Actual Values')
+        plt.legend()
+        plt.savefig(f"{path}/predicted_vs_actual.png", bbox_inches='tight', dpi=300)
+    else:
+        fig, ax = plt.subplots(figsize=(10, 6), nrows=y_pred.shape[1], ncols=1)
+        for i in range(y_pred.shape[1]):
+            # create outputs plot as subplots
+            ax[i].scatter(y_test[:, i], y_pred[:, i])
+            p1 = max(max(y_pred[:, i]), max(y_test[:, i]))
+            p2 = min(min(y_pred[:, i]), min(y_test[:, i]))
+            ax[i].plot([p1, p2], [p1, p2], 'k-')
+            ax[i].set_title(f"Predicted vs Actual Values for output {i}")
+        plt.xlabel('Actual')
+        plt.ylabel('Predicted')
+        # plt.legend()
+        plt.savefig(f"{path}/predicted_vs_actual.png", bbox_inches='tight', dpi=300)
+    if history is not None:
+        # Print the loss history
+        plt.figure(figsize=(10, 6))
+        plt.plot(history.history['loss'], label='Training Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.title('Loss History')
+        plt.legend()
+        plt.savefig(f"{path}/loss_history.png", bbox_inches='tight', dpi=300)
