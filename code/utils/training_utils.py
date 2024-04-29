f3af98d9189a78c397e239db3cc405b14580903d
Qamar-93 <qmff23@gmail.com>
2024-04-29 14:07:14 +0000

    custom loss callbacks

--- code/utils/training_utils.py
+++ code/utils/training_utils.py
@@ -1,17 +1,25 @@
 
 import tensorflow as tf
 
+def rescale_vectors(x, y):
+    min_val = tf.reduce_min(x)
+    max_val = tf.reduce_max(x)
+    x = tf.divide(tf.subtract(x, min_val), tf.subtract(max_val, min_val))
+    y = tf.divide(tf.subtract(y, min_val), tf.subtract(max_val, min_val))
+
+    return x, y
+
 class CustomLoss(tf.keras.losses.Loss):
     tf.keras.mixed_precision.set_global_policy('mixed_float16')
 
-    def __init__(self, model, metric, y_clean, x_noisy, len_input_features, bl_ratio, nominator, **kwargs):
+    # def __init__(self, model, metric, y_clean, x_noisy, len_input_features, bl_ratio, nominator, **kwargs):
+    def __init__(self, model, metric, y_clean, x_noisy, len_input_features, bl_ratio, **kwargs):
         super().__init__(**kwargs)
         self.model = model
         self.metric = metric
         self.x_noisy = x_noisy
         self.input_features = len_input_features
         self.bl_ratio = bl_ratio
-        self.nominator = nominator
         self.y_clean = y_clean
     
     
@@ -36,7 +44,8 @@ class CustomLoss(tf.keras.losses.Loss):
         
         return g_points
     def apply_model(self, x):
-        x_reshaped = tf.reshape(x, [-1, self.input_features])
+        # x_reshaped = tf.reshape(x, [-1, self.input_features])
+        x_reshaped = x
         return self.model(x_reshaped)
 
     def call(self, y_true, y_pred):
@@ -45,34 +54,110 @@ class CustomLoss(tf.keras.losses.Loss):
         
         y_noisy = tf.vectorized_map(self.apply_model, self.x_noisy)
         y_noisy_reshaped = tf.reshape(y_noisy, (self.x_noisy.shape[0], self.x_noisy.shape[1], 1))
-        gy = self.extract_g(y_noisy_reshaped, self.y_clean)
-        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - self.y_clean)))
+        gy = self.extract_g(y_noisy, self.y_clean)
+        # we have to rescale the following y_noisy_reshaped and y_clean using min-max scaling using the min and max of both vectors
+        y_clean_scaled, gy_scaled = rescale_vectors(self.y_clean, gy)
+        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy_scaled, tf.float64) - y_clean_scaled)))
         
         ratio = tf.math.divide(gy_y_dist, self.bl_ratio)
+        
         ratio = tf.cast(ratio, tf.float64)
         base_loss = tf.cast(base_loss, tf.float64)
-        loss = tf.math.add(base_loss, ratio)
-        # # tf.print("base_loss: ", base_loss, "loss: ", loss, "gy_y_dist: ", gy_y_dist, "ratio: ", ratio)
+        penalty = tf.math.multiply(base_loss, ratio)
+        loss = tf.math.add(base_loss, penalty)
         return loss
 
-## class for the validation loss using the black formula, and inheriting from the CustomLoss class
-class CustomLossBlack(CustomLoss):
+# customloss for dp
+class CustomLossDP(CustomLoss):
     def __init__(self, model, metric, y_clean, x_noisy, len_input_features, bl_ratio, nominator, **kwargs):
-        super().__init__(model, metric, y_clean, x_noisy, len_input_features, bl_ratio, nominator, **kwargs)
-    
+        super().__init__(model, metric, y_clean, x_noisy, len_input_features, bl_ratio, **kwargs)
+        self.nominator = nominator
     def call(self, y_true, y_pred):
         base_loss = tf.keras.losses.mean_squared_error(y_true, y_pred)
         y_noisy = tf.vectorized_map(self.apply_model, self.x_noisy)
-        y_noisy_reshaped = tf.reshape(y_noisy, (self.x_noisy.shape[0], self.x_noisy.shape[1], 1))
-        gy = self.extract_g(y_noisy_reshaped, self.y_clean)
-        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - self.y_clean)))
-        ratio = tf.math.divide(gy_y_dist, self.bl_ratio)
+
+        gy = self.extract_g(y_noisy, self.y_clean)
+        # we have to rescale the following y_noisy_reshaped and y_clean using min-max scaling using the min and max of both vectors
+        y_clean_scaled, gy_scaled = rescale_vectors(self.y_clean, gy)
+        # gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - self.y_clean)))
+        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy_scaled, tf.float64) - y_clean_scaled)))
+        
+        ratio = tf.math.divide(gy_y_dist, self.nominator)
+        
         ratio = tf.cast(ratio, tf.float64)
-        ratio = tf.math.add(tf.cast(tf.constant(1.), tf.float64), ratio)
-        ratio = tf.cast(ratio, tf.float32)
-        base_loss = tf.cast(base_loss, tf.float32)
-        loss = tf.math.multiply(base_loss, ratio)
+        base_loss = tf.cast(base_loss, tf.float64)
+        # loss = tf.math.add(base_loss, ratio)
+        penalty = tf.math.multiply(base_loss, ratio)
+        # penalty = tf.math.multiply(penalty, tf.cast(tf.constant(3.), tf.float64))
+        loss = tf.math.add(base_loss, penalty)
+        # tf.print("base_loss: ", base_loss, "loss: ", loss, "gy_y_dist: ", gy_y_dist, "bl_ratio: ", self.bl_ratio, "ratio: ", ratio)
         return loss
+
+class CustomCallback(tf.keras.callbacks.Callback):
+    def __init__(self, loss_instance):
+        super().__init__()
+        self.loss_instance = loss_instance
+
+    def on_epoch_end(self, epoch, logs=None):
+        y_noisy = tf.vectorized_map(self.loss_instance.apply_model, self.loss_instance.x_noisy)
+        y_noisy_reshaped = tf.reshape(y_noisy, (self.loss_instance.x_noisy.shape[0], self.loss_instance.x_noisy.shape[1], 1))
+        gy = self.loss_instance.extract_g(y_noisy_reshaped, self.loss_instance.y_clean)
+        y_clean_scaled, gy = rescale_vectors(self.loss_instance.y_clean, gy)
+        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - y_clean_scaled)))
+        
+        if gy_y_dist < self.loss_instance.bl_ratio:
+            self.model.stop_training = True
+
+class LossCallback(tf.keras.callbacks.Callback):
+    def __init__(self, x_noisy, model, y_clean, bl_ratio):
+        super().__init__()
+        self.x_noisy = x_noisy
+        self.model = model
+        self.y_clean = y_clean
+        self.bl_ratio = bl_ratio
+
+    def extract_g(self, y_noisy_tf, y_clean):
+        y_noisy_min = tf.reduce_min(y_noisy_tf, axis=0)
+        y_noisy_max = tf.reduce_max(y_noisy_tf, axis=0)
+        y_noisy_min = tf.cast(y_noisy_min, tf.float64)
+        y_noisy_max = tf.cast(y_noisy_max, tf.float64)
+        #### use L2 as the inner distance between y and the min/max bounds
+        y_dist_min = tf.sqrt(tf.square(y_noisy_min - y_clean))
+        y_dist_max = tf.sqrt(tf.square(y_noisy_max - y_clean))
+
+        gy = tf.maximum(y_dist_min, y_dist_max)
+        
+        mask_min = tf.equal(y_dist_min, gy)
+        mask_max = tf.equal(y_dist_max, gy)
+        g_points_min = tf.where(mask_min, y_noisy_min, tf.float32.min)
+        g_points_max = tf.where(mask_max, y_noisy_max, tf.float32.min)
+        g_points = tf.maximum(g_points_min, g_points_max)
+        
+        return g_points
+    def apply_model(self, x):
+        # x_reshaped = tf.reshape(x, [-1, self.input_features])
+        x_reshaped = x
+        return self.model(x_reshaped)
+    
+    def on_epoch_end(self, epoch, logs=None):
+        y_noisy = tf.vectorized_map(self.apply_model, self.x_noisy)
+        y_noisy_reshaped = tf.reshape(y_noisy, (self.x_noisy.shape[0], self.x_noisy.shape[1], 1))
+        gy = self.extract_g(y_noisy, self.y_clean)
+        # we have to rescale the following y_noisy_reshaped and y_clean using min-max scaling using the min and max of both vectors
+        y_clean_scaled, gy_scaled = rescale_vectors(self.y_clean, gy)
+        # gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - self.y_clean)))
+        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy_scaled, tf.float64) - y_clean_scaled)))
+        
+        ratio = tf.math.divide(gy_y_dist, self.bl_ratio)
+        base_loss = logs["loss"]
+        penalty = tf.math.multiply(base_loss, ratio)
+        # cast both to float64
+        base_loss = tf.cast(base_loss, tf.float64)
+        penalty = tf.cast(penalty, tf.float64)
+        loss = tf.math.add(base_loss, penalty)
+        logs["loss"] = loss
+        logs["mse"] = base_loss
+
         
 class CustomMetric(tf.keras.metrics.Metric):
     def __init__(self, model, y_clean, x_noisy, len_input_features, nominator, **kwargs):
@@ -111,12 +196,17 @@ class CustomMetric(tf.keras.metrics.Metric):
         y_noisy = tf.vectorized_map(self.apply_model, self.x_noisy)
         y_noisy_reshaped = tf.reshape(y_noisy, (self.x_noisy.shape[0], self.x_noisy.shape[1], 1))
         gy = self.extract_g(y_noisy_reshaped, self.y_clean)
-        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - self.y_clean)))
+        
+        # gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - self.y_clean)))
+        # rescale the vectors to be between 0 and 1
+        gy, y_clean_scaled = rescale_vectors(self.y_clean, gy)
+        gy_y_dist = tf.sqrt(tf.reduce_sum(tf.square(tf.cast(gy, tf.float64) - y_clean_scaled)))
+        
         ratio = tf.math.divide(self.nominator, gy_y_dist)
         ratio = tf.reduce_mean(ratio)
         ratio = tf.cast(ratio, tf.float32)
         self.ratio.assign(ratio)
-
+        # tf.print("ratio: ", self.ratio, "nominator: ", self.nominator, "gy_y_dist: ", gy_y_dist)
     def result(self):
         return self.ratio
 
